a0e5bfe480ddf32d992672a5186ab52771b2149b
Chad Stein <chadstein@Chads-MacBook-Pro.local>
2017-09-01 19:28:27 -0400

    lesson1, made the UI

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Chad Stein on 9/1/17.
+//  Copyright © 2017 Chad Stein. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
