921847f750ac7b3ade15e59f063055be0a9a2457
Chad Stein <chadstein@Chads-MacBook-Pro.local>
2017-09-03 20:32:56 -0400

    created struct & passed data

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Chad Stein on 9/3/17.
+//  Copyright © 2017 Chad Stein. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
